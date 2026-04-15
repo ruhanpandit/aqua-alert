4ee6f1553bae2982a8cd8d940907637118c4c897
Ruhan Pandit <ruhanpan@gmail.com>
2026-04-15 14:24:01 -0400

    add comments

--- main/main.ino
+++ main/main.ino
@@ -1,33 +1,40 @@
+// lcd setup
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// const variables for components
 const int BLUE_BUTTON = 2;
 const int GREEN_BUTTON = 3;
 const int BUZZER = 5;
 
+// variables to prevent repeating of text on lcd
 int mode = 0;
 int refills_left = 2;
 int lastModeShown = -1;
 bool refillShown = false;
 
+// buzzer variables
 bool buzzerOn = false;
 unsigned long lastBuzzToggle = 0;
 const unsigned long buzzInterval = 300; // ms
 
+// setup
 void setup() {
+    // assign component pins
     pinMode(BLUE_BUTTON, INPUT_PULLUP);
     pinMode(GREEN_BUTTON, INPUT_PULLUP);
     pinMode(BUZZER, OUTPUT);
 
+    // initalize lcd
     lcd.init();
     lcd.backlight();
-
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Select mode");
     lcd.setCursor(0, 1);
     lcd.print("B:refill G:timer");
 
+    // user selects mode (refill or timer)
     while (mode == 0) {
         if (digitalRead(BLUE_BUTTON) == HIGH) {
             mode = 1;
@@ -37,10 +44,13 @@ void setup() {
         }
     }
 
+    // clear lcd to prepare for tracking
     lcd.clear();
     delay(500);
 }
 
+// method for controlling buzzer in the background
+// on/off/on/off functionality similar to an alarm clock
 void updateBuzzer() {
     if (refills_left == 0 && refillShown) {
         unsigned long now = millis();
@@ -60,7 +70,9 @@ void updateBuzzer() {
     }
 }
 
+// refill mode
 void refill() {
+    // print refills left
     if (refills_left > 0 && lastModeShown != 1) {
         lcd.clear();
         lcd.setCursor(0, 0);
@@ -70,12 +82,14 @@ void refill() {
         lastModeShown = 1;
     }
 
+    // remove refills if blue button is pressed
     if (refills_left > 0 && digitalRead(BLUE_BUTTON) == HIGH) {
         delay(200);
         refills_left--;
         lastModeShown = -1;
     }
 
+    // display reset message if no refills left
     if (refills_left == 0 && !refillShown) {
         lcd.clear();
         lcd.setCursor(0, 0);
@@ -85,6 +99,7 @@ void refill() {
         refillShown = true;
     }
 
+    // turn off buzzer if user acknowledge buzzer
     if (digitalRead(GREEN_BUTTON) == HIGH && refills_left == 0 && refillShown) {
         refills_left = 2;
         refillShown = false;
@@ -92,6 +107,8 @@ void refill() {
         noTone(BUZZER);
     }
 
+    // constantly call this function
+    // function checks if no refills left and starts buzzer accordingly
     updateBuzzer();
 }
 
@@ -107,6 +124,7 @@ void timer() {
 }
 
 void loop() {
+    // choose mode and constantly loop through
     if (mode == 1) {
         refill();
     }
